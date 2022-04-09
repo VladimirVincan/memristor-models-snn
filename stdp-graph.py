46b36c8283b259574eadbe4328b4f6de1b231065
unknown <vladimirvincan@gmail.com>
2022-04-10 00:26:49 +0200

    update stdp generation

--- stdp-graph.py
+++ stdp-graph.py
@@ -8,14 +8,23 @@ l.parse()
 
 diff = []
 time = []
-time_val = 0.1
+time_val = 0
+time_mid = 0.5
+
+# ---- EDIT ----
+time_start = 0.01
+time_update = 0.01
+# ---- end EDIT ----
+
 print(l.case_count)
 for i in range(l.case_count): # Iteration in simulation cases
     V_y = l.get_data('V(y)', i)
     diff.append(-V_y[0] + V_y[-1])
-    time.append(time_val)
-    time_val += 0.1
+    time.append(time_start + time_val - time_mid)
+    time_val += time_update
     print (i)
+plt.plot(time, diff, 'bo')
+plt.ylabel('Voltage change Δy (V)')
+plt.xlabel(r'Inter-pulse interval $t_{gap}$ (s)')
 print(diff, time)
-plt.plot(time, diff)
 plt.show()
